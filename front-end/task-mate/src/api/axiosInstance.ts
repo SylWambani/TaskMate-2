16c69d5b14becee5e2605bb1530e9a2f5c2929ef
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2025-12-04 16:41:13 +0300

    Update axiosInstance

--- front-end/task-mate/src/api/axiosInstance.ts
+++ front-end/task-mate/src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // your backend base URL
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
@@ -32,7 +33,7 @@ axiosInstance.interceptors.response.use(
 
       try {
         const res = await axios.post(
-          "http://127.0.0.1:8000/auth/jwt/refresh/",
+          `${import.meta.env.VITE_API_URL}/auth/jwt/refresh/`,
           {
             refresh,
           }
